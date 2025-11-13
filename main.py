3cadbe9f396510f412ee7056a5ad55edaf65105e
Gravetat <79274520+Gravetat@users.noreply.github.com>
2025-11-13 16:31:19 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
+#numero1 = float(input ("Ingrese un numero "))
+#numero2 = float (input ("ingrese otro numero"))
+
+#respuesta = numero1 + numero2 
+
+#print("El resultado final es ", respuesta)
+
+#tiempo extra
+
+#Toma dos números y resta el segundo del primero.
+
 numero1 = float(input ("Ingrese un numero "))
 numero2 = float (input ("ingrese otro numero"))
 
-respuesta = numero1 + numero2 
-
+respuesta = numero1 - numero2 
 print("El resultado final es ", respuesta)
+
+#Toma dos números y multiplícalos.
+
+numero1 = float(input ("Ingrese un numero "))
+numero2 = float (input ("ingrese otro numero"))
+
+respuesta = numero1 * numero2 
+print("El resultado final es ", respuesta)
+
+#Toma dos números y divide el primero entre el segundo.
+
+numero1 = float(input ("Ingrese un numero "))
+numero2 = float (input ("ingrese otro numero"))
+
+respuesta = numero1 / numero2 
+print("El resultado final es ", respuesta)
+
+#Toma dos números y realiza una operación de módulo.
+numero1 = float(input ("Ingrese un numero "))
+numero2 = float (input ("ingrese otro numero"))
+
+respuesta = numero1 % numero2 
+print("El resultado final es ", respuesta)
+
+#Permitir a los usuarios elegir qué operación desean realizar con dos números.
+
+
 
 
